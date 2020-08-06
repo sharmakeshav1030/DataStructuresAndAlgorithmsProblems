ec06182d0b373f3c6421291a57a6aeee53d2ac1d
Keshav Sharma <33628783+sharmakeshav1030@users.noreply.github.com>
2020-08-06 15:10:54 +0530

    Create ReverseString.java

---
+++ Arrays/ReverseString.java
@@ -0,0 +1,17 @@
+/*
+ * This is the java class which basically reverses the given string word wise.
+ */
+ 
+public class ReverseString {
+	public static String reverseWordWise(String input) {
+		
+        String word[] = input.split(" ");
+		String reverse = "";
+
+		for (int i = word.length - 1; i >= 0; i--) {
+			reverse += word[i] + " ";
+		}
+        
+        return reverse;
+	}
+}
